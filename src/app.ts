02897b65f197e3218aa0e63f7b515bc57c8508a3
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-06-13 11:21:50 -0400

    more generic prac

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,35 @@
 //     data.split(' ');
 // })
 
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-const mergeObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
-  { name: "Max", hobbies: ["Sports"] },
-  { age: 30 }
-);
+const mergeObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 
 console.log(mergeObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+extractAndConvert({ name: "Max" }, "name");
